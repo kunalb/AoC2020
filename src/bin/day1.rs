1dd681347682ad17d220b4acf5948e1e3283cce3
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-12-01 01:02:42 -0500

    Remove unnecessary Result

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,27 +2,27 @@ use std::io::{self, Read};
 use std::error::Error;
 use std::collections::HashSet;
 
-fn solve1(nums: &[i64]) -> Result<i64, Box<dyn Error>> {
+fn solve1(nums: &[i64]) -> i64 {
     let mut set: HashSet<i64> = HashSet::new();
     set.extend(nums);
 
     for a in nums {
         if set.contains(&(2020 - a)) {
-            return Ok(a * (2020 - a));
+            return a * (2020 - a);
         }
     }
 
     panic!("Solution 1: Couldn't find valid numbers!");
 }
 
-fn solve2(nums: &[i64]) -> Result<i64, Box<dyn Error>> {
+fn solve2(nums: &[i64]) -> i64{
     let mut set: HashSet<i64> = HashSet::new();
     set.extend(nums);
 
     for a in nums {
         for b in nums {
             if set.contains(&(2020 - a - b)) {
-                return Ok(a * b * (2020 - a - b));
+                return a * b * (2020 - a - b);
             }
         }
     }
@@ -40,7 +40,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
     let nums = parse(&buffer);
-    println!("1: {}\n2: {}", solve1(&nums)?, solve2(&nums)?);
+    println!("1: {}\n2: {}", solve1(&nums), solve2(&nums));
 
     Ok(())
 }
@@ -58,15 +58,11 @@ mod test {
 
     #[test]
     fn test1() {
-        let result = solve1(&parse(INPUT));
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), 514579);
+        assert_eq!(solve1(&parse(INPUT)), 514579);
     }
 
     #[test]
     fn test2() {
-        let result = solve2(&parse(INPUT));
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), 241861950);
+        assert_eq!(solve2(&parse(INPUT)), 241861950);
     }
 }
