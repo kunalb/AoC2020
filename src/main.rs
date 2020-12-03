2f14d8d0032c92da8c0d2bb6c4b68da634f93073
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-12-02 23:24:17 -0500

    Improve template

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,26 @@ use std::io::{self, Read};
 use std::error::Error;
 use std::env;
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn parse(line: &str) -> Result<&str, Box<dyn Error>> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"(?P<cap>.*)"#).unwrap();
+    }
+    let captures = RE.captures(line).unwrap();
+    let result = captures.name("cap").unwrap().as_str();
+    Ok(result)
+}
+
 fn solve1(buffer: &str) -> Result<String, Box<dyn Error>> {
-    Ok(String::from("Solution 1"))
+    Ok(format!("Solution 1: {}", parse(buffer)?))
 }
 
 fn solve2(buffer: &str) -> Result<String, Box<dyn Error>> {
-    Ok(String::from("Solution 2"))
+    Ok(format!("Solution 2: {}", parse(buffer)?))
 }
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
