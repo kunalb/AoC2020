9b8ae7d3b505f3cee4fb1935e3fd973daaaf0fce
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-12-11 01:03:57 -0500

    Horrible version Day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,41 +1,56 @@
-use std::io::{self, Read};
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::io::{self, Read};
+
+fn parse(buffer: &str) -> Vec<Vec<char>> {
+    buffer
+        .lines()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
 
 fn solve1(buffer: &str) -> Result<String, Box<dyn Error>> {
-    let mut grid = buffer.lines()
-        .map(|x| x.as_bytes().to_vec())
-        .collect::<Vec<_>>();
+    let mut grid = parse(buffer);
 
     loop {
         let mut changes = 0;
-        let mut nextgrid: Vec<Vec<u8>> = Vec::new();
+        let mut nextgrid: Vec<Vec<char>> = Vec::new();
 
         for row in grid.iter().enumerate() {
             let mut new_row = Vec::new();
             for col in row.1.iter().enumerate() {
                 let mut occ = 0;
 
-                for i in 0..3 {
-                    for j in 0..3 {
-                        if i == 1 && j == 1 {
+                for i in -1..1 {
+                    for j in -1..1 {
+                        if i == 0 && j == 0 {
                             continue;
                         }
 
-                        let r: i32 = row.0 as i32 + i - 1;
-                        let c: i32 = col.0 as i32 + j - 1;
+                        let r: i32 = row.0 as i32 + i;
+                        let c: i32 = col.0 as i32 + j;
 
-                        if r >= 0 && c >= 0 && (r as usize) < grid.len()  && (c as usize) < row.1.len() && grid[r as usize][c as usize] == '#' as u8 {
+                        if r >= 0
+                            && c >= 0
+                            && (r as usize) < grid.len()
+                            && (c as usize) < row.1.len()
+                            && grid[r as usize][c as usize] == '#'
+                        {
                             occ += 1;
                         }
                     }
                 }
 
-
                 new_row.push(match *col.1 as char {
-                    'L' if occ == 0 => { changes += 1; '#' as u8 },
-                    '#' if occ >= 4 => { changes += 1; 'L' as u8 },
-                    x => x as u8,
+                    'L' if occ == 0 => {
+                        changes += 1;
+                        '#'
+                    }
+                    '#' if occ >= 4 => {
+                        changes += 1;
+                        'L'
+                    }
+                    x => x,
                 });
             }
             nextgrid.push(new_row);
@@ -46,7 +61,7 @@ fn solve1(buffer: &str) -> Result<String, Box<dyn Error>> {
             let mut total = 0;
             for row in grid {
                 for col in row {
-                    if col == '#' as u8 {
+                    if col == '#' {
                         total += 1;
                     }
                 }
@@ -58,7 +73,8 @@ fn solve1(buffer: &str) -> Result<String, Box<dyn Error>> {
 }
 
 fn solve2(buffer: &str) -> Result<String, Box<dyn Error>> {
-    let mut grid = buffer.lines()
+    let mut grid = buffer
+        .lines()
         .map(|x| x.as_bytes().to_vec())
         .collect::<Vec<_>>();
 
@@ -84,10 +100,19 @@ fn solve2(buffer: &str) -> Result<String, Box<dyn Error>> {
                             r += dr;
                             c += dc;
 
-                            if r >= 0 && c >= 0 && (r as usize) < grid.len() && (c as usize) < row.1.len() {
+                            if r >= 0
+                                && c >= 0
+                                && (r as usize) < grid.len()
+                                && (c as usize) < row.1.len()
+                            {
                                 match grid[r as usize][c as usize] as char {
-                                    'L' => { break; }
-                                    '#' => { occ += 1; break;}
+                                    'L' => {
+                                        break;
+                                    }
+                                    '#' => {
+                                        occ += 1;
+                                        break;
+                                    }
                                     _ => {}
                                 }
                             } else {
@@ -98,8 +123,14 @@ fn solve2(buffer: &str) -> Result<String, Box<dyn Error>> {
                 }
 
                 new_row.push(match *col.1 as char {
-                    'L' if occ == 0 => { changes += 1; '#' as u8 },
-                    '#' if occ >= 5 => { changes += 1; 'L' as u8 },
+                    'L' if occ == 0 => {
+                        changes += 1;
+                        '#' as u8
+                    }
+                    '#' if occ >= 5 => {
+                        changes += 1;
+                        'L' as u8
+                    }
                     x => x as u8,
                 });
             }
@@ -123,7 +154,6 @@ fn solve2(buffer: &str) -> Result<String, Box<dyn Error>> {
     }
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let now = std::time::Instant::now();
 
@@ -131,7 +161,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     io::stdin().read_to_string(&mut buffer)?;
 
     let args: Vec<String> = env::args().collect();
-    if args.len() >1 && args[1] == "2" {
+    if args.len() > 1 && args[1] == "2" {
         println!("{}", solve2(&buffer)?);
     } else {
         println!("{}", solve1(&buffer)?);
@@ -140,7 +170,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     eprintln!("Time: {}ms", now.elapsed().as_millis());
     Ok(())
 }
-
 
 #[cfg(test)]
 mod test {
