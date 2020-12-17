ebccb5e96da54e16c0733518cac2326ddf7bdcaa
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-12-17 03:57:50 -0500

    Cleaned up Day 17

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -2,235 +2,143 @@ use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::io::{self, Read};
-use itertools::iproduct;
 
-fn solve(buffer: &str, dims: usize) {
+struct Neighbors {
+    ranges: Vec<(i64, i64)>,
+    state: Vec<i64>,
+    counter: usize,
+    total: usize,
+    dims: usize,
+}
+
+impl Neighbors {
+    fn new(root: &Vec<i64>) -> Neighbors {
+        Neighbors::new_ranges(root.iter().map(|x| (x - 1, x + 1)).collect::<Vec<_>>())
+    }
+
+    fn new_ranges(ranges: Vec<(i64, i64)>) -> Neighbors {
+        let dims = ranges.len();
+        let total = ranges.iter().map(|(a, b)| (b - a + 1) as usize).product();
+        let state = ranges.iter().map(|(a, _)| *a).collect::<Vec<_>>();
+
+        Neighbors {
+            ranges,
+            dims,
+            counter: 0,
+            total,
+            state,
+        }
+    }
+}
+
+impl Iterator for Neighbors {
+    type Item = Vec<i64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.counter >= self.total {
+            return None;
+        }
+        self.counter += 1;
+
+        let result = Some(self.state.clone());
+
+        for i in (0..self.dims).rev() {
+            self.state[i] += 1;
+            if self.state[i] > self.ranges[i].1 {
+                self.state[i] = self.ranges[i].0
+            } else {
+                break;
+            }
+        }
+
+        result
+    }
+}
+
+fn print_cube(hypercube: &HashMap<Vec<i64>, bool>, ranges: &Vec<(i64, i64)>) {
+    for outer_path in Neighbors::new_ranges(ranges[..ranges.len() - 2].to_vec()) {
+        println!("{:?}", outer_path);
+        let y_range = ranges[ranges.len() - 2];
+        let x_range = ranges[ranges.len() - 1];
+        for y in y_range.0..=y_range.1 {
+            for x in x_range.0..=x_range.1 {
+                let mut path = outer_path.clone();
+                path.push(y);
+                path.push(x);
+                let pt = hypercube.get(&path).unwrap_or(&false);
+                print!("{} ", if *pt { '#' } else { '.' });
+            }
+            println!();
+        }
+
+        println!();
+    }
+}
+
+fn solve(buffer: &str, dims: usize) -> usize {
+    assert!(dims >= 2);
+
     let mut hypercube: HashMap<Vec<i64>, bool> = HashMap::new();
-    let mut ranges: HashMap<usize, (i64, i64)> = HashMap::new();
+    let mut next = hypercube.clone();
+
+    let mut ranges: Vec<(i64, i64)> = vec![(0, 0); dims];
 
     for (y, line) in buffer.lines().enumerate() {
         for (x, ch) in line.chars().enumerate() {
-            let path = vec![0; dims];
-            path[dims - 1] = x;
-            path[dims - 2] = y;
+            let mut path = vec![0 as i64; dims];
+            path[dims - 1] = x as i64;
+            path[dims - 2] = y as i64;
 
-            let pt = hypercube.entry(path)
-                .or_insert(false);
+            let pt = hypercube.entry(path).or_insert(false);
             *pt = match ch {
                 '.' => false,
                 '#' => true,
                 _ => unreachable!(),
             };
 
-            x_range.1 = x as i64;
+            ranges[dims - 1].1 = x as i64;
         }
 
-        y_range.1 = y as i64;
+        ranges[dims - 2].1 = y as i64;
     }
-}
 
-fn solve1(buffer: &str) -> Result<usize, Box<dyn Error>> {
-    let mut cube: HashMap<i64, HashMap<i64, HashMap<i64, bool>>> = HashMap::new();
-    let mut next_cube = cube.clone();
+    for _ in 0..6 {
+        let current_ranges = ranges.iter().map(|(a, b)| (a - 1, b + 1)).collect::<Vec<_>>();
+        ranges = vec![(0, 0); dims];
 
-    let mut z_range: (i64, i64) = (0, 0);
-    let mut x_range: (i64, i64) = (0, 0);
-    let mut y_range: (i64, i64) = (0, 0);
+        for path in Neighbors::new_ranges(current_ranges) {
+            let current = hypercube.get(&path).unwrap_or(&false);
+            let neighbors = Neighbors::new(&path)
+                .filter(|n| *n != path)
+                .filter_map(|n| hypercube.get(&n))
+                .filter(|a| **a)
+                .count();
 
-    for (y, line) in buffer.lines().enumerate() {
-        for (x, ch) in line.chars().enumerate() {
-            let pt = cube
-                .entry(0)
-                .or_insert(HashMap::new())
-                .entry(y as i64)
-                .or_insert(HashMap::new())
-                .entry(x as i64)
-                .or_insert(false);
-            *pt = match ch {
-                '.' => false,
-                '#' => true,
-                _ => unreachable!(),
+            let next_state = match (current, neighbors) {
+                (true, 2) | (_, 3) => true,
+                _ => false,
             };
 
-            x_range.1 = x as i64;
-        }
-
-        y_range.1 = y as i64;
-    }
-
-    for cycle in 0..6 {
-        for z in (z_range.0 - 1)..=(z_range.1 + 1) {
-            for y in (y_range.0 - 1)..=(y_range.1 + 1) {
-                for x in (x_range.0 - 1)..=(x_range.1 + 1) {
-                    let pt = next_cube
-                        .entry(z)
-                        .or_insert(HashMap::new())
-                        .entry(y)
-                        .or_insert(HashMap::new())
-                        .entry(x)
-                        .or_insert(false);
-
-                    let current = cube
-                        .get(&z)
-                        .and_then(|ys| ys.get(&y))
-                        .and_then(|xs| xs.get(&x))
-                        .unwrap_or(&false);
-
-                    let neighbors = iproduct!(z - 1..=z + 1, y - 1..=y + 1, x - 1..=x + 1)
-                        .filter(|(zz, yy, xx)| *zz != z || *yy != y || *xx != x)
-                        .filter_map(|(zz, yy, xx)| {
-                            cube.get(&zz)
-                                .and_then(|ys| ys.get(&yy))
-                                .and_then(|xs| xs.get(&xx))
-                        })
-                        .filter(|a| **a)
-                        .count();
-
-                    *pt = match (current, neighbors) {
-                        (true, 2) | (true, 3) => true,
-                        (false, 3) => true,
-                        _ => false,
-                    };
-
-                    if *pt {
-                        z_range.0 = if z_range.0 > z { z } else { z_range.0 };
-                        z_range.1 = if z_range.1 < z { z } else { z_range.1 };
-                        y_range.0 = if y_range.0 > y { y } else { y_range.0 };
-                        y_range.1 = if y_range.1 < y { y } else { y_range.1 };
-                        x_range.0 = if x_range.0 > x { x } else { x_range.0 };
-                        x_range.1 = if x_range.1 < x { x } else { x_range.1 };
+            if next_state {
+                for i in 0..dims {
+                    if path[i] > ranges[i].1 {
+                        ranges[i].1 = path[i];
+                    } else if path[i] < ranges[i].0 {
+                        ranges[i].0 = path[i];
                     }
                 }
             }
+
+            next.insert(path, next_state);
         }
 
-        let temp = cube;
-        cube = next_cube;
-        next_cube = temp;
+        let temp = hypercube;
+        hypercube = next;
+        next = temp;
+        // print_cube(&hypercube, &ranges);
     }
 
-    Ok(cube
-        .values()
-        .map(|ys| ys.values())
-        .flatten()
-        .map(|xs| xs.values())
-        .flatten()
-        .filter(|a| **a)
-        .count())
-}
-
-fn solve2(buffer: &str) -> Result<usize, Box<dyn Error>> {
-    let mut cube: HashMap<i64, HashMap<i64, HashMap<i64, HashMap<i64, bool>>>> = HashMap::new();
-    let mut next_cube = cube.clone();
-
-    let mut w_range: (i64, i64) = (0, 0);
-    let mut z_range: (i64, i64) = (0, 0);
-    let mut x_range: (i64, i64) = (0, 0);
-    let mut y_range: (i64, i64) = (0, 0);
-
-    for (y, line) in buffer.lines().enumerate() {
-        for (x, ch) in line.chars().enumerate() {
-            let pt = cube
-                .entry(0)
-                .or_insert(HashMap::new())
-                .entry(0)
-                .or_insert(HashMap::new())
-                .entry(y as i64)
-                .or_insert(HashMap::new())
-                .entry(x as i64)
-                .or_insert(false);
-            *pt = match ch {
-                '.' => false,
-                '#' => true,
-                _ => unreachable!(),
-            };
-
-            x_range.1 = x as i64;
-        }
-
-        y_range.1 = y as i64;
-    }
-
-    for cycle in 0..6 {
-        let w_range2 = w_range.clone();
-        let z_range2 = z_range.clone();
-        let y_range2 = y_range.clone();
-        let x_range2 = x_range.clone();
-
-        w_range = (0, 0);
-        z_range = (0, 0);
-        y_range = (0, 0);
-        x_range = (0, 0);
-
-        for w in (w_range2.0 - 1)..=(w_range2.1 + 1) {
-            for z in (z_range2.0 - 1)..=(z_range2.1 + 1) {
-                for y in (y_range2.0 - 1)..=(y_range2.1 + 1) {
-                    for x in (x_range2.0 - 1)..=(x_range2.1 + 1) {
-                        let pt = next_cube
-                            .entry(w)
-                            .or_insert(HashMap::new())
-                            .entry(z)
-                            .or_insert(HashMap::new())
-                            .entry(y)
-                            .or_insert(HashMap::new())
-                            .entry(x)
-                            .or_insert(false);
-
-                        let current = cube
-                            .get(&w)
-                            .and_then(|zs| zs.get(&z))
-                            .and_then(|ys| ys.get(&y))
-                            .and_then(|xs| xs.get(&x))
-                            .unwrap_or(&false);
-
-                        let neighbors = iproduct!(w - 1..=w + 1, z - 1..=z + 1, y - 1..=y + 1, x - 1..=x + 1)
-                                .filter(|(ww, zz, yy, xx)| {
-                                    *ww != w || *zz != z || *yy != y || *xx != x
-                                })
-                                .filter_map(|(ww, zz, yy, xx)| {
-                                    cube.get(&ww)
-                                        .and_then(|zs| zs.get(&zz))
-                                        .and_then(|ys| ys.get(&yy))
-                                        .and_then(|xs| xs.get(&xx))
-                                })
-                                .filter(|a| **a)
-                                .count();
-
-                        *pt = match (current, neighbors) {
-                            (true, 2) | (true, 3) => true,
-                            (false, 3) => true,
-                            _ => false,
-                        };
-
-                        if *pt {
-                            w_range.0 = if w_range.0 > w { w } else { w_range.0 };
-                            w_range.1 = if w_range.1 < w { w } else { w_range.1 };
-                            z_range.0 = if z_range.0 > z { z } else { z_range.0 };
-                            z_range.1 = if z_range.1 < z { z } else { z_range.1 };
-                            y_range.0 = if y_range.0 > y { y } else { y_range.0 };
-                            y_range.1 = if y_range.1 < y { y } else { y_range.1 };
-                            x_range.0 = if x_range.0 > x { x } else { x_range.0 };
-                            x_range.1 = if x_range.1 < x { x } else { x_range.1 };
-                        }
-                    }
-                }
-            }
-        }
-        let temp = cube;
-        cube = next_cube;
-        next_cube = temp;
-    }
-
-    Ok(cube
-        .values()
-        .map(|zs| zs.values())
-        .flatten()
-        .map(|ys| ys.values())
-        .flatten()
-        .map(|xs| xs.values())
-        .flatten()
-        .filter(|a| **a)
-        .count())
+    hypercube.values().filter(|a| **a).count()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -241,9 +149,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 && args[1] == "2" {
-        println!("{}", solve2(&buffer)?);
+        println!("{}", solve(&buffer, 4));
     } else {
-        println!("{}", solve1(&buffer)?);
+        println!("{}", solve(&buffer, 3));
     }
 
     eprintln!("Time: {}ms", now.elapsed().as_millis());
@@ -258,13 +166,14 @@ mod test {
 ..#
 ###";
 
+
     #[test]
     fn test1() {
-        assert_eq!(solve1(INPUT).unwrap(), 112);
+        assert_eq!(solve(INPUT, 3), 112);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve2(INPUT).unwrap(), 848);
+        assert_eq!(solve(INPUT, 4), 848);
     }
 }
